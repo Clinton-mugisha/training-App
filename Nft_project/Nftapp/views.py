94aef4b34df1f1809d8fad96aa05d13f378eae3e
TrinityTushabe <trinitytushabe@gmail.com>
2024-01-23 21:37:39 +0300

    updates on the model, views and forms

--- Nft_project/Nftapp/views.py
+++ Nft_project/Nftapp/views.py
@@ -2,7 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.shortcuts import render, HttpResponse
 from django.views import View
-from .models import Job, Applicant
+from .models import Job, Applicant, Resume
 from .forms import JobForm, ApplicantForm, ResumeForm
 from .ml_module import train_model, rank_resumes # Import the ml_module functions
 
@@ -57,13 +57,57 @@ def create_resume(request):
     if request.method == 'POST':
         form = ResumeForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            # Get the form data
+            full_name = form.cleaned_data['full_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            education = form.cleaned_data['education']
+            skills = form.cleaned_data['skills']
+            work_experience = form.cleaned_data['work_experience']
+            languages = form.cleaned_data['languages']
+            upload_cv = form.cleaned_data['upload_cv']
+
+            # Check if 'Other' is selected in languages
+            other_language = form.cleaned_data.get('other_language', None)
+            if 'Other' in languages and other_language:
+                # Do something with the other_language, for example, print it
+                print(f"Other Language Specified: {other_language}")
+
+            # Create and save the Resume instance
+            resume = Resume(
+                full_name=full_name,
+                phone_number=phone_number,
+                email=email,
+                education=education,
+                skills=skills,
+                work_experience=work_experience,
+                languages=languages,
+                other_language=other_language,
+                upload_cv=upload_cv
+            )
+            resume.save()
+
             # You can add a success message or redirect to a thank-you page
             return HttpResponse("Thank you for submitting your resume!")
+
     else:
         form = ResumeForm()
 
     return render(request, 'create_resume.html', {'form': form})
+
+
+
+# def create_resume(request):
+#     if request.method == 'POST':
+#         form = ResumeForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             # You can add a success message or redirect to a thank-you page
+#             return HttpResponse("Thank you for submitting your resume!")
+#     else:
+#         form = ResumeForm()
+
+#     return render(request, 'create_resume.html', {'form': form})
 
 
 # def create_resume(request):
